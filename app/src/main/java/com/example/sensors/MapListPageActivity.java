af348cfe2201f526e9fe2a6852e224d3c4816ab7
Michelle <michawest@bk.ru>
2025-05-24 11:37:14 +0300

    Added saving data between session

--- app/src/main/java/com/example/sensors/MapListPageActivity.java
+++ app/src/main/java/com/example/sensors/MapListPageActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.hardware.SensorPrivacyManager;
 import android.os.Bundle;
@@ -16,28 +17,30 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapListPageActivity extends AppCompatActivity {
     private ListView listOfMap;
-    private ArrayList<Field> fields = new ArrayList<>();
+    private ArrayList<Field> fields;
+    private static final String PREFS_NAME = "MapListPagePrefs";
+    private static final String FIELDS_LIST_KEY = "fields_list";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_list_page);
 
+        loadDataFromPrefs();
+
         listOfMap = findViewById(R.id.map_list_view);
-
-        fields.add(new Field("Michelle"));
-        fields.add(new Field("Matvei"));
-        fields.add(new Field("Dascha"));
-
         MyAdapter adapter = new MyAdapter(this, fields);
         listOfMap.setAdapter(adapter);
 
@@ -63,6 +66,12 @@ public class MapListPageActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        saveDataToPrefs();
+    }
+
     private void shoeAddItemDialog(){
         CreateFieldDialog dialog = new CreateFieldDialog(this, new CreateFieldDialog.CustomDialogListener() {
             @Override
@@ -77,6 +86,30 @@ public class MapListPageActivity extends AppCompatActivity {
         });
 
         dialog.show();
+    }
+
+    private void loadDataFromPrefs(){
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefs.getString(FIELDS_LIST_KEY, null);
+
+        Type type = new TypeToken<ArrayList<Field>>() {}.getType();
+        fields = gson.fromJson(json, type);
+
+        if(fields == null){
+            fields = new ArrayList<>();
+        }
+    }
+
+    private void saveDataToPrefs(){
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        Gson gson = new Gson();
+        String json = gson.toJson(fields);
+
+        editor.putString(FIELDS_LIST_KEY, json);
+        editor.apply();
     }
 
     public class MyAdapter extends ArrayAdapter<Field>{
@@ -117,7 +150,6 @@ public class MapListPageActivity extends AppCompatActivity {
             return convertView;
         }
     }
-
 
 
 }
