ab181031c68520da47dff4e0b05c72756ac19119
Michelle <michawest@bk.ru>
2025-06-17 23:41:37 +0300

    Made the GetCoordinatedFieldPage

---
+++ app/src/main/java/com/example/sensors/GetCoordinatesFieldActivity.java
@@ -0,0 +1,13 @@
+package com.example.sensors;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GetCoordinatesFieldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
